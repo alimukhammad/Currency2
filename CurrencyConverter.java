495ce9d2ce2db59271dc4038bca057b3a670c214
sardor <alimukhammad1@gmail.com>
2023-04-13 23:50:25 -0400

    euro to dollar at the rate of 1.20 euro > 1 us dollar

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CurrencyConverter{
     private static final double EXCHANGE_RATE = 1.20;//1 euro = 1.20 US Dollar
 
@@ -5,5 +7,14 @@ public class CurrencyConverter{
         return euros * EXCHANGE_RATE;
     }
 
-    
+    public static void main(String[] args) {
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter amount of Euros to convert: ");
+            double euros = scanner.nextDouble();
+
+            double usd = convertEuroToUSD(euros);
+
+            System.out.println(euros + " Euros = " + usd + " US dollars");
+        }
+    }
 }
